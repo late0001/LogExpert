3ebe064f00a5d963b75c8a6ded7eb3db56eb070c
jdqiu <jdqiu@sina.cn>
2026-05-11 21:38:27 +0800

    csv bugfixes and benchmarks

--- src/LogExpert.UI/Controls/LogWindow/LogWindow.cs
+++ src/LogExpert.UI/Controls/LogWindow/LogWindow.cs
@@ -3461,7 +3461,10 @@ internal partial class LogWindow : DockContent, ILogPaintContextUI, ILogView, IL
             _ = columnComboBox.Items.Add(columnName);
         }
 
-        columnComboBox.SelectedIndex = 0;
+        if (columnComboBox.Items.Count > 0)
+        {
+            columnComboBox.SelectedIndex = 0;
+        }
 
         OnColumnizerChanged(CurrentColumnizer);
     }
