d2bfb47b3dcdb6480eb611bd546aedf6a5b7042c
jingdong_qiu <jingdong_qiu@realtek.com>
2026-06-03 14:07:48 +0800

    update led not showing after start

--- src/LogExpert.UI/Dialogs/LogTabWindow/LogTabWindow.cs
+++ src/LogExpert.UI/Dialogs/LogTabWindow/LogTabWindow.cs
@@ -628,6 +628,12 @@ internal partial class LogTabWindow : Form, ILogTabWindow
 
         _ledService.RegisterWindow(logWindow);
 
+        if (logWindow.Tag is LogWindowData ledData)
+        {
+            var icon = GetLedIcon(ledData.LedState.DiffSum, ledData);
+            _ = BeginInvoke(SetTabIcon, logWindow, icon);
+        }
+
         ConnectEventHandlers(logWindow);
     }
 
